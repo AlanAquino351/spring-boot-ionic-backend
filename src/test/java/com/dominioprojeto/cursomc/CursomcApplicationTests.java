2f3e0d8cbd9ca2d1103efcb185e652055a147d0c
AlanAquino351 <alanaquinocosta@hotmail.com>
2021-11-09 12:10:31 -0300

    Projeto criado

---
+++ src/test/java/com/dominioprojeto/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominioprojeto.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
